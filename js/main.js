b4947c301f1bf3cde5286ec528809c6253ede3f8
Anton Savelev <dpssinet@gmail.com>
2019-06-14 00:42:03 +0300

    Генерация объявлений

---
+++ js/main.js
@@ -0,0 +1,67 @@
+'use strict';
+var COUNT_ARRAY = 8;
+var OFFER_TYPE = ['place', 'flat', 'house', 'bungalo'];
+var MIN_MAP_Y = 130;
+var MAX_MAP_Y = 630;
+var adsArray = [];
+var mapBlock = document.querySelector('.map');
+var mapPin = document.querySelector('.map__pin--main');
+var mapWidth = mapBlock.offsetWidth;
+var pinWidth = mapPin.offsetWidth;
+
+
+// Генерация рандомного числа для от min до max включительно
+function getRandomNumber(min, max) {
+  var rand = min - 0.5 + Math.random() * (max - min + 1);
+  rand = Math.round(rand);
+  return rand;
+}
+
+// Генерация адреса изображения
+function getImagePath(number) {
+  var imageNumber = number < 10 ? '0' + number : number;
+  return {avatar: 'img/avatars/user' + imageNumber + '.png'};
+}
+
+// Генерация типа offer
+function getOfferType(typeOfferArr, randomNumber) {
+  return {type: typeOfferArr[randomNumber]};
+}
+
+// Генерация координат X, Y
+function getCoordinatePinX(widthMap, widthPin, minY, maxY) {
+  return {x: getRandomNumber(widthPin, widthMap - widthPin), y: getRandomNumber(minY, maxY)};
+}
+
+
+// Заполнение массива похожих объявлений
+for (var i = 0; i < COUNT_ARRAY; i++) {
+  adsArray.push({
+    author: getImagePath(i + 1),
+    offer: getOfferType(OFFER_TYPE, getRandomNumber(0, OFFER_TYPE.length - 1)),
+    location: getCoordinatePinX(mapWidth, pinWidth, MIN_MAP_Y, MAX_MAP_Y)
+  });
+}
+console.log(adsArray);
+
+mapBlock.classList.remove('map--faded');
+var pinblock = document.querySelector('.map__pins');
+var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+var fragment = document.createDocumentFragment();
+
+// Заполнение указателей
+function renderAds(ad) {
+  var pinElem = pinTemplate.cloneNode(true);
+  pinElem.style.left = '' + ad.location.x + 'px';
+  pinElem.style.top = '' + ad.location.y + 'px';
+  pinElem.querySelector('img').src = '' + ad.author.avatar + '';
+  pinElem.querySelector('img').alt = 'заголовок объявления';
+  return pinElem;
+}
+
+// Добавление элементов в фрагмент
+for (var j = 0; j < adsArray.length; j++) {
+  fragment.appendChild(renderAds(adsArray[j]));
+}
+
+pinblock.appendChild(fragment);
