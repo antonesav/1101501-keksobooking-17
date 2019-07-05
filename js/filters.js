157efbf163dc6fbdfce2ef2507075ce5ebdfa096
anton_work <dpssinet@gmail.com>
2019-07-05 18:52:08 +0300

    remodel

--- js/filters.js
+++ js/filters.js
@@ -1,34 +1,46 @@
 'use strict';
 (function () {
+  var Price = {
+    LOW: 1000,
+    MEDIUM: 10000,
+    HIGH: 100000
+  };
   var filtersBlock = document.querySelector('.map__filters');
   var mapPin = document.querySelector('.map__pin--main');
+  var filterParams = {};
+
   function housingChangeHandler(evt) {
-    var changeValue = evt.target.value;
-    changeFilters(changeValue);
+    filterParams[evt.target.name] = evt.target.value;
+    console.log(filterParams);
+    changeFilters(filterParams.name);
+    //var fiteredList = filterOffers(array, filterParams);
   }
 
+  // function initFilters(){
   filtersBlock.addEventListener('change', housingChangeHandler);
+  // }
 
   function changeFilters(value) {
     var filterByType = window.globalUtils.copyAdsArray.filter(function (item) {
       return item.offer.type === value;
     });
-    var filterByRooms = window.globalUtils.copyAdsArray.filter(function (item) {
-      return item.offer.rooms === value;
-    });
-    var filterByPrice = window.globalUtils.copyAdsArray.filter(function (item) {
-      return item.offer.price === value;
-    });
-    var filterByGuests = window.globalUtils.copyAdsArray.filter(function (item) {
-      return item.offer.guests === value;
-    });
+    // var filterByRooms = window.globalUtils.copyAdsArray.filter(function (item) {
+    //   return item.offer.rooms === Number(value);
+    // });
+    // var filterByPrice = window.globalUtils.copyAdsArray.filter(function (item) {
+    //   return item.offer.price === value;
+    // });
+    // var filterByGuests = window.globalUtils.copyAdsArray.filter(function (item) {
+    //   return item.offer.guests === Number(value);
+    // });
 
-    var filteredAds = filterByType.concat(filterByRooms).concat(filterByPrice).concat(filterByGuests).concat(window.globalUtils.copyAdsArray);
+    var filteredAds = filterByType.concat(window.globalUtils.copyAdsArray);
     var uniqueAds =
       filteredAds.filter(function (it, i) {
         return filteredAds.indexOf(it) === i;
       });
     removePinBlockChild();
+    console.log(uniqueAds);
     window.cardUtils.renderAds(uniqueAds);
   }
 
