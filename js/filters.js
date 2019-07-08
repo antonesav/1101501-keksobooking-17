5f33580569272d7d0336b09e87333a6ac75c269c
Anton Savelev <dpssinet@gmail.com>
2019-07-09 00:28:12 +0300

    Фильтрация features

--- js/filters.js
+++ js/filters.js
@@ -2,35 +2,56 @@
 (function () {
   var filtersBlock = document.querySelector('.map__filters');
   var mapPin = document.querySelector('.map__pin--main');
+  var filterParams = {};
+  var checkedFeatures = {};
   function housingChangeHandler(evt) {
-    var changeValue = evt.target.value;
-    changeFilters(changeValue);
+    var name = evt.target.name;
+    filterParams[name] = evt.target.value;
+    filterOffers(window.globalUtils.copyAdsArray, name, filterParams[name]);
+  }
+
+  function filterOffers(array, name, paramValue) {
+    var initialAds = array;
+    var filteredAds;
+
+    filteredAds = initialAds.filter(function (item) {
+      if (filterParams[name] === 'any') {
+        return item;
+      }
+      return item.offer[name].toString() === filterParams[name];
+    });
+
+    if (filterParams.features) {
+      filteredAds = getCheckedFeatures(array, paramValue);
+    }
+
+    removePinBlockChild();
+    window.cardUtils.renderAds(filteredAds);
+  }
+
+  function getCheckedFeatures(array, valueElement) {
+    var checkedFeatureElements = [];
+
+    if (checkedFeatures[valueElement]) {
+      checkedFeatures[valueElement] = false;
+    } else {
+      checkedFeatures[valueElement] = true;
+    }
+
+    array.forEach(function (elem) {
+      var arrayOffers = elem.offer.features;
+      var filteredFeatures = arrayOffers.filter(function (item) {
+        return checkedFeatures['' + item + ''] === true;
+      });
+      if (arrayOffers.length && arrayOffers.length === filteredFeatures.length) {
+        checkedFeatureElements.push(elem);
+      }
+    });
+    return checkedFeatureElements;
+    // console.log(checkedFeatureElements);
   }
 
   filtersBlock.addEventListener('change', housingChangeHandler);
-
-  function changeFilters(value) {
-    var filterByType = window.globalUtils.copyAdsArray.filter(function (item) {
-      return item.offer.type === value;
-    });
-    var filterByRooms = window.globalUtils.copyAdsArray.filter(function (item) {
-      return item.offer.rooms === value;
-    });
-    var filterByPrice = window.globalUtils.copyAdsArray.filter(function (item) {
-      return item.offer.price === value;
-    });
-    var filterByGuests = window.globalUtils.copyAdsArray.filter(function (item) {
-      return item.offer.guests === value;
-    });
-
-    var filteredAds = filterByType.concat(filterByRooms).concat(filterByPrice).concat(filterByGuests).concat(window.globalUtils.copyAdsArray);
-    var uniqueAds =
-      filteredAds.filter(function (it, i) {
-        return filteredAds.indexOf(it) === i;
-      });
-    removePinBlockChild();
-    window.cardUtils.renderAds(uniqueAds);
-  }
 
   function removePinBlockChild() {
     Array.from(window.globalUtils.pinBlock.children).forEach(function (pinNode) {
